eb38b687794503cdb6fd213bdbea976ed0aec350
paraslov <paraslovjm@gmail.com>
2021-08-06 19:53:46 +0300

    refactoring...

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const corsOptions = {
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer')
 
 const smtp_user = process.env.SMTP_USER
 const smtp_password = process.env.SMTP_PASSWORD
@@ -22,12 +22,12 @@ const smtp_password = process.env.SMTP_PASSWORD
 
 // create reusable transporter object using the default SMTP transport
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    service: 'gmail',
     auth: {
-        user: 'sergeybalanov.folio@gmail.com', // generated ethereal user
-        pass: 'FoLio3795Sb', // generated ethereal password
+        user: smtp_user, // generated ethereal user
+        pass: smtp_password, // generated ethereal password
     },
-});
+})
 
 
 app.get('/', cors(corsOptions), (req, res) => {
@@ -39,21 +39,25 @@ app.post('/send-message', async (req, res) => {
     const {name, email, message} = req.body
 
     // send mail with defined transport object
-    const info = await transporter.sendMail({
-        from: '"Job Alert!!" <sergeybalanov.folio@gmail.com>', // sender address
-        to: "paraslovjm@gmail.com, tsfoe@mail.ru", // list of receivers
-        subject: `${name} send you a message.`, // Subject line
-        // text: "Hello world?", // plain text body
-        html: `<b>You have a message from ${name}!</b>
+    try {
+        const info = await transporter.sendMail({
+            from: '"Job Alert!!" <sergeybalanov.folio@gmail.com>', // sender address
+            to: 'paraslovjm@gmail.com, tsfoe@mail.ru', // list of receivers
+            subject: `${name} send you a message.`, // Subject line
+            // text: "Hello world?", // plain text body
+            html: `<b>You have a message from ${name}!</b>
             <div>Message: ${message}</div>
             <div>${name} email is: ${email}</div>`, // html body
-    });
+        })
+    } catch (err) {
+        res.send({message: 'something goes wrong', error: err})
+    }
 
-    res.send({message: 'email send success'})
+    res.send({message: 'email send success', body: req.body})
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`App listening`)
 })
 
 
